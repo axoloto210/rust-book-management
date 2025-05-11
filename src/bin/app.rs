8de29e826c2d8c6a22aaa4aa5a83753c4f78d6a9
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-05-11 18:29:34 +0900

    app.rsで各DIコンテナを使用するように

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,55 +1,32 @@
+use adapter::database::connect_database_with;
+use anyhow::{Error, Result};
+use api::route::health::build_health_check_routers;
+use axum::Router;
+use registry::AppRegistry;
+use shared::config::AppConfig;
 use std::net::{Ipv4Addr, SocketAddr};
-
-use axum::{extract::State, http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
-
-use anyhow::Result;
-
-use sqlx::{PgPool, postgres::PgConnectOptions};
-
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    bootstrap().await
+}
 
-    let database_cfg = DatabaseConfig {
-        host: "localhost".into(),
-        port: 5432,
-        username: "app".into(),
-        password: "passwd".into(),
-        database: "app".into(),
-    };
+async fn bootstrap() -> Result<()> {
+    let app_config = AppConfig::new()?;
+    let pool = connect_database_with(&app_config.database);
 
-    let conn_pool = connect_database_with(database_cfg);
+    let registry = AppRegistry::new(pool);
 
-
-    let app = Router::new().route("/health", get(health_check))
-    .route("/health/db", get(health_check_db))
-    .with_state(conn_pool);
+    let app = Router::new()
+        .merge(build_health_check_routers())
+        .with_state(registry);
 
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
 
-    let listner = TcpListener::bind(addr).await?; //?演算子はその時点でエラーが出ると呼び出し元の関数へとエラーを伝播させる。
+    let listener = TcpListener::bind(addr).await?; //?演算子はその時点でエラーが出ると呼び出し元の関数へとエラーを伝播させる。
 
     println!("Listening on {}", addr);
 
-    Ok(axum::serve(listner, app).await?)
-}
-
-#[tokio::test]
-async fn health_check_works() {
-    let status_code = health_check().await;
-
-    assert_eq!(status_code, StatusCode::OK);
-}
-
-#[sqlx::test]
-async fn health_check_db_works(pool: sqlx::PgPool) {
-    let status_code  = health_check_db(State(pool)).await;
-    assert_eq!(status_code, StatusCode::OK);
-}
-
-#[tokio::test]
-async fn check_hello_world_works() {
-    let str = hello_world().await;
-    assert_eq!(str,"Hello World!")
+    axum::serve(listener, app).await.map_err(Error::from)
 }
