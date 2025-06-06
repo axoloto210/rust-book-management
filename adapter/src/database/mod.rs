2989b3eb2c4f794dfa7cacf72f5b4b2822012661
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-06-06 21:03:19 +0900

    adapterにDB関連の処理を追加

--- adapter/src/database/mod.rs
+++ adapter/src/database/mod.rs
@@ -2,6 +2,8 @@ use shared::config::DatabaseConfig;
 
 use sqlx::{postgres::PgConnectOptions, PgPool};
 
+pub mod model;
+
 fn make_pg_connect_options(cfg: &DatabaseConfig) -> PgConnectOptions {
     PgConnectOptions::new()
         .host(&cfg.host)
